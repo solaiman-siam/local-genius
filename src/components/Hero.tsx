82736c88f6dc40d1ede33f2f3eb48ed283d6e739
solaiman-siam <solaimansiam93@gmail.com>
2025-06-18 22:34:22 +0600

    added box shadow

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,20 +10,19 @@ function Hero() {
 
         <div className="flex flex-col items-center gap-8">
           <h1 className="text-5xl font-medium">
-            {" "}
             Trusted Local Discovery, Instantly by Text or Chat.
           </h1>
           <p className="w-8/12 text-lg text-center text-description">
             No apps, No endless scrolling. Just the best small businesses and
             outdoor experiences- handpicked , vetted and delivered instantly by
-            your AI-Powered local assistant.{" "}
+            your AI-Powered local assistant.
           </p>
 
           <button className="bg-primaryBg text-white px-6 py-3 rounded-full ">
             Start Your Free Trial
           </button>
         </div>
-        <div className="grid grid-cols-4 gap-6">
+        <div className="grid  drop-shadow-bloom grid-cols-4 gap-6">
           <div className="flex flex-col gap-3 p-7 bg-white rounded-2xl">
             <div className="rounded-full bg-white p-1">
                 <Image src={Bot} alt="bot" />
@@ -57,7 +56,7 @@ function Hero() {
               businesses.
             </p>
           </div>
-          <div className="flex flex-col gap-3 p-7 bg-white rounded-2xl">
+          <div className="flex drop-shadow-bloom flex-col gap-3 p-7 bg-white rounded-2xl">
             <div className="rounded-full bg-white p-1">
                 <Image src={Bot} alt="bot" />
             </div>
@@ -68,10 +67,6 @@ function Hero() {
               businesses.
             </p>
           </div>
-        </div>
-
-        <div className="absolute top-1/2 left-1/2 size-96 shadow-primaryBg/20 drop-shadow-bloom bg-primaryBg ">
-
         </div>
       </div>
     </Container>
